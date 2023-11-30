75238e93cd2045c14cfa06dbc3341cc33f1ca2a8
Lee-chan0 <yab0403@kakao.com>
2023-11-30 19:48:19 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-//
 
 app.get("/", (req, res) => {
   res.json({ message: "success!!!!!" });
